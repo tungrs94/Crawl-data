e85ad9a79ddb8be8f4ccfae0ef0f100932e35b71
Nguyễn Bá Tùng <tungrs94@gmail.com>
2020-08-19 22:58:07 +0700

    first commit

---
+++ repo-githubUser.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import sys
+
+
+def get_repos(user):
+    '''Get all GitHub repositories of user
+    '''
+    r = requests.get("http://api.github.com/users/{}/repos"
+                     .format(user))
+    data = json.loads(r.text)
+    repos = [item['name'] for item in data]
+
+    return repos
+
+
+def main():
+    user = sys.argv[1]
+    repos = get_repos(user)
+    print("GitHub User: {} has {} repositories:"
+          .format(user, len(repos)))
+    for repo in repos:
+        print("-", repo)
+
+
+if __name__ == "__main__":
+    main()
